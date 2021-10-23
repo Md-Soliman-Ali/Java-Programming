22124ba32405e932c4231dbf9825d15db84e2fe5
Md. Soliman Ali <soliman15-8572@diu.edu.bd>
2021-10-23 11:10:26 +0600

    Initial commit

---
+++ src/main/java/basic/Hello_Java.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Hello_Java {
+    public static void main(String[] args) {
+        // System.out.println("Hello World");
+
+        String myVar = "Hello World";
+        System.out.println(myVar);
+    }
+}
---
+++ src/main/java/variable/Java_Variable.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Java_Variable {
+
+    static String profession = "Software Automation Engineer."; // static variable
+
+    public static void main(String[] args) {
+        String name = "Md. Soliman Ali."; // instance variable
+        System.out.println("I am " +name+ " I am a " +profession);
+        experience();
+    }
+
+    static void experience() {
+        int exp = 2; // local variable
+        System.out.println("I have " +exp+ " years of hands on experience.");
+    }
+
+}
